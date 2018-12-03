a159db1718b30e397cdb13717df08255cbdfe821
Mike. Shi <ashida2016@outlook.com>
2018-12-03 16:53:14 +0800

    updated 08

--- source/08-if.py
+++ source/08-if.py
@@ -5,3 +5,30 @@
 
 # 目的:
 # 掌握使用 if 语句
+
+# 简单 if 语句
+num = float(input("输入一个数字: "))
+if num>0:
+    print("正数")
+else:
+    print("负数")
+
+# 内嵌 if 语句
+num = float(input("输入一个数字: "))
+if num >= 0:
+   if num == 0:
+       print("零")
+   else:
+       print("正数")
+else:
+   print("负数")
+
+# elif 的使用
+# 用户输入数字
+num = float(input("输入一个数字: "))
+if num > 0:
+   print("正数")
+elif num == 0:
+   print("零")
+else:
+   print("负数")
