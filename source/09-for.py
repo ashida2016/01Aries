8bcb8106790684e020300457b9aac33b22a7812c
Mike. Shi <ashida2016@outlook.com>
2018-12-04 09:27:15 +0800

    updated 09

--- source/09-for.py
+++ source/09-for.py
@@ -9,6 +9,8 @@
 
 # 输出指定范围内的素数
 # take input from the user
+print("-----华丽分割线-----")
+print("输出指定范围内所有整数：")
 lower = int(input("输入区间最小值: "))
 upper = int(input("输入区间最大值: "))
 for num in range(lower,upper + 1):
@@ -17,6 +19,8 @@ for num in range(lower,upper + 1):
 
 # 输出指定范围内的素数
 # take input from the user
+print("-----华丽分割线-----")
+print("输出指定范围内的素数：")
 lower = int(input("输入区间最小值: "))
 upper = int(input("输入区间最大值: "))
 
