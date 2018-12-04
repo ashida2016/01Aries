b7456b0e4a3c52248103369c2a56edb1a7787d9d
Mike. Shi <ashida2016@outlook.com>
2018-12-04 12:29:52 +0800

    updated 10

--- source/09-for.py
+++ source/09-for.py
@@ -6,6 +6,10 @@
 # 目的:
 # 掌握使用for语句
 
+# 最简单的 for
+for i in range(1, 5) :
+    print(i)
+
 
 # 输出指定范围内的素数
 # take input from the user
@@ -13,7 +17,7 @@ print("-----华丽分割线-----")
 print("输出指定范围内所有整数：")
 lower = int(input("输入区间最小值: "))
 upper = int(input("输入区间最大值: "))
-for num in range(lower,upper + 1):
+for num in range(lower, upper + 1):
     print(num)
 
 
@@ -24,10 +28,10 @@ print("输出指定范围内的素数：")
 lower = int(input("输入区间最小值: "))
 upper = int(input("输入区间最大值: "))
 
-for num in range(lower,upper + 1):
+for num in range(lower, upper + 1):
     # 素数大于 1
     if num > 1:
-        for i in range(2,num):
+        for i in range(2, num):
             if (num % i) == 0:
                 break
         else:
---
+++ source/10-pass.py
@@ -0,0 +1,43 @@
+# -*- coding: UTF-8 -*-
+
+# Filename : 10-pass.py
+# author by : （学员ID)
+
+# 目的:
+# 汇总掌握01-09的知识点
+
+# 导入 random(随机数) 模块
+import random
+import datetime
+
+
+# 生成第一个人物 ID，武力值，并准备成字符串
+id = random.randint(1000, 1999)
+force_value = random.randint(10, 100)
+character = "角色id=" + str(id) + " 武力值=" + str(force_value) + "\n"
+
+# 写文件
+filename = "test.txt"
+f = open(filename, 'w')  # write 方式第一次写一行
+text2write = character
+f.write(text2write)
+f.close()
+
+#生成 n 个人物ID及武力值
+# 输出指定范围内的素数
+# take input from the user
+for i in range(1, 10000):
+    # 生成第 i 个人物 ID，武力值，并准备成字符串
+    id = random.randint(2000, 2999)
+    force_value = random.randint(10, 100)
+    now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    #character = "日期时间：" + str(now) + "角色id=" + str(id) + " 武力值=" + str(force_value) + "\n"
+    character = "日期时间：" + now + " 角色id=" + str(id) + " 武力值=" + str(force_value) + "\n"
+
+    # 写文件
+    filename = "test.txt"
+    f = open(filename, 'a')  # 追加方式一次加一行
+    text2write = character
+    f.write(text2write)
+
+f.close()
