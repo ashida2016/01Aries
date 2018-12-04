99a368835169c294608aba8c91adb521550df16e
Mike. Shi <ashida2016@outlook.com>
2018-12-04 23:08:16 +0800

    updated 05

--- source/05-string.py
+++ source/05-string.py
@@ -9,31 +9,31 @@
 
 #  -------------------------------
 # 练习一
-str = input("输入一串全部小写的英语：")
-print("全部转化为大写后：%s\n" % (str.upper()))          # 把所有字符中的小写字母转换成大写字母
+s = input("输入一串全部小写的英语：")
+print("全部转化为大写后：%s\n" % (s.upper()))            # 把所有字符中的小写字母转换成大写字母
 
-str = input("输入一串全部大写的英语：")
-print("全部转化为大写后：%s\n" % (str.lower()))          # 把所有字符中的大写字母转换成小写字母
+s = input("输入一串全部大写的英语：")
+print("全部转化为大写后：%s\n" % (s.lower()))            # 把所有字符中的大写字母转换成小写字母
 
-str = input("输入一串全部小写的英语句子：")
-print("首字母转化为大写后：%s\n" % (str.capitalize()))     # 把第一个字母转化为大写字母，其余小写
+s = input("输入一串全部小写的英语句子：")
+print("首字母转化为大写后：%s\n" % (s.capitalize()))     # 把第一个字母转化为大写字母，其余小写
 
-str = input("输入一串全部小写的英语句子（必须带空格）：")
-print("每个单词首字母转化为大写后：%s\n" % (str.title()))          # 把每个单词的第一个字母转化为大写，其余小写
+s = input("输入一串全部小写的英语句子（必须带空格）：")
+print("每个单词首字母转化为大写后：%s\n" % (s.title()))   # 把每个单词的第一个字母转化为大写，其余小写
 
 
 # 练习二
 print("-----华丽分割线-----")
-str = input("请任意输入一串英语句子：")
-print(str.islower()) # 判断所有字符都是小写
+s = input("请任意输入一串英语句子：")
+print(s.islower()) # 判断所有字符都是小写
 
-# print(str.isalnum()) # 判断所有字符都是数字或者字母
-# print(str.isalpha()) # 判断所有字符都是字母
-# print(str.isdigit()) # 判断所有字符都是数字
-# print(str.islower()) # 判断所有字符都是小写
-# print(str.isupper()) # 判断所有字符都是大写
-# print(str.istitle()) # 判断所有单词都是首字母大写，像标题
-# print(str.isspace()) # 判断所有字符都是空白字符、\t、\n、\r
+# print(s.isalnum()) # 判断所有字符都是数字或者字母
+# print(s.isalpha()) # 判断所有字符都是字母
+# print(s.isdigit()) # 判断所有字符都是数字
+# print(s.islower()) # 判断所有字符都是小写
+# print(s.isupper()) # 判断所有字符都是大写
+# print(s.istitle()) # 判断所有单词都是首字母大写，像标题
+# print(s.isspace()) # 判断所有字符都是空白字符、\t、\n、\r
 
 # 练习三
 print("-----华丽分割线-----")
