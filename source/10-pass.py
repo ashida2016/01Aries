e0af77814e53e0b40a1501795f6dc024c79256bc
Mike. Shi <ashida2016@outlook.com>
2018-12-04 12:41:44 +0800

    updated 10

--- source/10-pass.py
+++ source/10-pass.py
@@ -14,7 +14,7 @@ import datetime
 # 生成第一个人物 ID，武力值，并准备成字符串
 id = random.randint(1000, 1999)
 force_value = random.randint(10, 100)
-character = "角色id=" + str(id) + " 武力值=" + str(force_value) + "\n"
+character = "第一个角色id=" + str(id) + " 武力值=" + str(force_value) + "\n"
 
 # 写文件
 filename = "test.txt"
@@ -24,9 +24,12 @@ f.write(text2write)
 f.close()
 
 #生成 n 个人物ID及武力值
-# 输出指定范围内的素数
-# take input from the user
-for i in range(1, 10000):
+# 打开文件准备写入
+filename = "test.txt"
+f = open(filename, 'a')  # 追加方式一次加一行
+
+# 生成 n 个 人物并写入文件
+for i in range(1, 100):
     # 生成第 i 个人物 ID，武力值，并准备成字符串
     id = random.randint(2000, 2999)
     force_value = random.randint(10, 100)
@@ -34,10 +37,9 @@ for i in range(1, 10000):
     #character = "日期时间：" + str(now) + "角色id=" + str(id) + " 武力值=" + str(force_value) + "\n"
     character = "日期时间：" + now + " 角色id=" + str(id) + " 武力值=" + str(force_value) + "\n"
 
-    # 写文件
-    filename = "test.txt"
-    f = open(filename, 'a')  # 追加方式一次加一行
+    # 写入一个人物
     text2write = character
     f.write(text2write)
 
+# 关闭文件
 f.close()
